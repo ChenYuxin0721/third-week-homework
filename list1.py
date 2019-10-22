55ec67efaa6ffa34cc48141c196cb04d91e9e038
ChenYuxin0721 <1349074040@qq.com>
2019-10-22 16:16:51 +0800

    first commit

---
+++ list1.py
@@ -0,0 +1,19 @@
+student_basic_info = [
+    (1, "6662118001", "张三"),
+    (2, "6662118002", "李四"),
+    (3, "6662118051", "王五"),
+    (4, "6662118052", "赵六")
+]
+
+student_class_info = [
+    ("6662118001", "计算机201班"),
+    ("6662118002", "计算机202班"),
+    ("6662118051", "计算机202班"),
+    ("6662118052", "计算机203班")
+]
+
+L = [student_basic_info[i]+ student_class_info[i] for i in range(0,len(student_basic_info))]
+L = [str(a)+','+str(b)+','+c+','+e for a,b,c,d,e in L]
+
+for info in L:
+    print(info)
